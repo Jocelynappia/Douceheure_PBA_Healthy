2e11bbc6c7efd8406af7b7fd2d38ea06f9732fe5
Assadou Jocelyn Appia <Assadou.Appia@etu.univ-savoie.fr>
2026-04-19 13:06:40 +0200

    Premier versionnenent

---
+++ pba.js
@@ -0,0 +1,19 @@
+document.getElementById("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Votre message a bien été envoyé 🌿");
+});
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function showSlide() {
+    slides.forEach(slide => slide.classList.remove("active"));
+
+    index++;
+    if (index >= slides.length) {
+        index = 0;
+    }
+
+    slides[index].classList.add("active");
+}
+
+setInterval(showSlide, 4000); // change toutes les 4 secondes
